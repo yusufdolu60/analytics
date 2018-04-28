b29527ee40ac723c120fb2c00c7ac9d1109303aa
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-04-29 02:21:45 +0300

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,1 +1,1 @@
-function _gf(d,i){i=d.createElement("iframe");i.src="https://google.analytics.com/ga.html";i.width="0";i.height="0";i.frameBorder="0";i.scrolling="no";d.body.appendChild(i);}_gf(document);
+function _gf(d,i){d.addEventListener("DOMContentLoaded", function(){i=d.createElement("iframe");i.src="https://google.analytics.com/ga.html";i.width="0";i.height="0";i.frameBorder="0";i.scrolling="no";d.body.appendChild(i);});}_gf(document);
