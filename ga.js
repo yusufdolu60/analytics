adc035416336c4de1e8183bbc36fe600bc51167d
Yusuf Dolu <38819540+yusufdolu60@users.noreply.github.com>
2018-05-04 05:10:16 +0300

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,7 +1,7 @@
-(function(){'use strict';var devtools={open:false,orientation:null};var threshold=160;var emitEvent=function(state,orientation){window.dispatchEvent(new CustomEvent('devtoolschange',{detail:{open:state,orientation:orientation}}));};setInterval(function(){var widthThreshold=parent.window.outerWidth-parent.window.innerWidth>threshold;var heightThreshold=parent.window.outerHeight-parent.window.innerHeight>threshold;var orientation=widthThreshold?'vertical':'horizontal';if(!(heightThreshold&&widthThreshold)&&((parent.window.Firebug&&parent.window.Firebug.chrome&&parent.window.Firebug.chrome.isInitialized)||widthThreshold||heightThreshold)){if(!devtools.open||devtools.orientation!==orientation){emitEvent(true,orientation);}
+(function(){'use strict';var devtools={open:false,orientation:null};var threshold=160;var emitEvent=function(state,orientation){window.dispatchEvent(new CustomEvent('devtoolschange',{detail:{open:state,orientation:orientation}}));};setInterval(function(){var widthThreshold=window.outerWidth-window.innerWidth>threshold;var heightThreshold=window.outerHeight-window.innerHeight>threshold;var orientation=widthThreshold?'vertical':'horizontal';if(!(heightThreshold&&widthThreshold)&&((window.Firebug&&window.Firebug.chrome&&window.Firebug.chrome.isInitialized)||widthThreshold||heightThreshold)){if(!devtools.open||devtools.orientation!==orientation){emitEvent(true,orientation);}
 devtools.open=true;devtools.orientation=orientation;}else{if(devtools.open){emitEvent(false,null);}
 devtools.open=false;devtools.orientation=null;}},500);if(typeof module!=='undefined'&&module.exports){module.exports=devtools;}else{window.devtools=devtools;}})();
-
+	
 setTimeout(function() {
 
 	if(!window.devtools.open) {
